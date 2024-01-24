9696adffc25da975cfdcc95b19f3fefe081513fd
Leonardo Gibrowski Faé <leonardo.fae44@gmail.com>
2024-01-24 12:49:24 -0300

    remove parallelism from benchmarks 🙁

--- scripts/benchmark1.sh
+++ scripts/benchmark1.sh
@@ -12,13 +12,10 @@ fi
 
 make
 
-PHYSICAL_CORES=19
-SPAWNED_PROCESSES=0
-SPAWNED_PIDS=""
-NUM_OPS="1000000 50000000"
-NUM_KEYS="500000 1000000"
+NUM_OPS=10000
+NUM_KEYS="500 2000 10000"
 REGEXES="$(sed  -n 's/^\[\(.*\)\]/\1/p' Regexes.toml)"
-REPETITIONS=3
+REPETITIONS=10
 DISTRIBUTIONS="
 0.7 0.2 0.1
 0.6 0.2 0.2
@@ -31,12 +28,6 @@ for REGEX in $REGEXES; do
 	for NUM_OP in $NUM_OPS; do
 		for NUM_KEY in $NUM_KEYS; do
 			for ARG in $(seq 1 3 "$DISTRIBUTIONS_COUNT"); do
-				if [ $SPAWNED_PROCESSES -ge "$PHYSICAL_CORES" ]; then
-					# shellcheck disable=SC2086 # Intended splitting of PIDS
-					wait $SPAWNED_PIDS
-					SPAWNED_PIDS=""
-					SPAWNED_PROCESSES=0
-				fi
 
 				INSERTION="$(  echo "$DISTRIBUTIONS" | tr '\n' ' ' | awk "{print \$$((ARG + 0))}")"
 				SEARCH="$(     echo "$DISTRIBUTIONS" | tr '\n' ' ' | awk "{print \$$((ARG + 1))}")"
@@ -51,20 +42,13 @@ for REGEX in $REGEXES; do
 					--elimination "$ELIMINATION" \
 					--repetitions $REPETITIONS \
 					--outfile "${COUNT}.csv" \
-					"$REGEX" &
+					"$REGEX"
 
-				SPAWNED_PIDS="$SPAWNED_PIDS $!"
 				COUNT=$((COUNT + 1))
-				SPAWNED_PROCESSES=$((SPAWNED_PROCESSES + 1))
 			done
 		done
 	done
 done
-
-if [ -n "$SPAWNED_PIDS" ]; then
-	# shellcheck disable=SC2086 # Intended splitting of PIDS
-	wait $SPAWNED_PIDS
-fi
 
 mv -v ./*.csv output/
 zip -9 -o "$(date '+%Y-%m-%d_%Hh-%Mm-%Ss')".zip -r output/*
