23f2d2a93bc0e1babe8868ee83afb1ecefede976
renatobhf <renatowbhf@gmail.com>
2024-01-30 18:55:22 -0300

    uniformity analysis

--- scripts/plot_distribution.py
+++ scripts/plot_distribution.py
@@ -13,7 +13,7 @@ args = parser.parse_args()
 try:
     arrays = importlib.import_module(args.module.replace(".py","")) 
 except ImportError:
-    print(args.module, "not found! Please make sure that the module exists and is in the same directory as this script or you are passing the absolute path of the array file.")
+    print(args.module, "not found! Please make sure that the module exists and is in the same directory as this script.")
     print("If it does not exist, you can get it by running keyuser with --test-distribution and redirect to", args.module)
     exit(1)
 
@@ -24,28 +24,41 @@ import numpy as np
 def goodness_fit_test(key, value):
     key = key.replace("array_","")
     # Calculate the observed frequencies of each value
-    observed_freq = np.bincount(abs(value))
+    observed_freq, _bin_edges = np.histogram(value, bins=len(np.unique(value)))
 
     # Calculate the expected frequencies for a uniform distribution
     expected_freq = np.full_like(observed_freq, len(value) / len(np.unique(value)))
 
     # Perform the Chi-Square Goodness of Fit Test
-    chi2, p = stats.chisquare(observed_freq, expected_freq)
+    try:
+        chi2, p = stats.chisquare(observed_freq, expected_freq)
+    except Exception as e:
+        print("Chi-Test failed for key ", key)
+        p = 0.0
 
     skewness = stats.skew(value)
-    print(f" {key} skewness: {skewness}")
-    print(f" {key} Chi-Square: {chi2}")
-    print(f" {key} P-value: {p}")
+    if abs(skewness) > 1.0:
+        print(f" {key:<20} skewness:   {skewness:.2f} --> Highly skewed. ")
+    elif abs(skewness) > 0.5:
+        print(f" {key:<20} skewness:   {skewness:.2f} --> Moderately skewed. ")
+    else:
+        print(f" {key:<20} skewness:   {skewness:.2f} --> Approximately symmetric. ")
+
+    if p > 0.05:
+        print(f" {key:<20} p-value:    {p:.2f} --> SYMMETRIC")
+    else:
+        print(f" {key:<20} p-value:    {p:.2f} --> ASSYMETRIC")
 
 def main():
-    _, ax = plt.subplots()
+    _, ax = plt.subplots(figsize=(10, 5))
 
     for key,value in arrays.distributions.items():
         ax.hist(value, label=key, alpha=0.5)
         goodness_fit_test(key,value)
 
-    ax.legend()
+    ax.legend(loc='upper left', bbox_to_anchor=(1, 1))
 
+    plt.tight_layout()
     plt.savefig('distributions.svg')
 
 main()
