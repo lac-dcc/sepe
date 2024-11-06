7ea42c7f31ca2f1f04ce97f2bfe3e7bfde2ba09d
Renato Hoffmann <renatowbhf@gmail.com>
2024-11-06 17:35:57 -0300

    rq7 script artifact

--- artifact/rq7_benchmark.sh
+++ artifact/rq7_benchmark.sh
@@ -2,13 +2,15 @@
 
 set -e
 
-if [ "$(basename "$(pwd)")" = "scripts" ]; then
+if [ "$(basename "$(pwd)")" = "scripts" ] || [ "$(basename "$(pwd)")" = "artifact"  ]; then
 	cd ..
 fi
 
 if [ ! -d output ]; then
 	mkdir output
 fi
+
+git checkout upper-shift-experiment
 
 UPPER_SHIFTS="0 16 24 32 48"
 NUM_OPS=10000
@@ -23,6 +25,8 @@ DISTRIBUTIONS="
 DISTRIBUTIONS_COUNT="$(echo "$DISTRIBUTIONS" | wc -w)"
 
 HISTOGRAM_DISTRIBUTION="normal uniform incremental"
+
+mkdir -p temp-files
 
 for HD in $HISTOGRAM_DISTRIBUTION; do
 	for SHIFT in $UPPER_SHIFTS; do
@@ -60,5 +64,49 @@ for HD in $HISTOGRAM_DISTRIBUTION; do
 		done
 		mv -v ./*.csv temp-files/
 	done
-	zip -9 -o "histogram-${HD}.zip" -r temp-files/*
+	zip -9 -o "rq7-${HD}.zip" -r temp-files/*
 done
+
+rm -f temp-files/*
+
+for SHIFT in $UPPER_SHIFTS; do
+	make clean
+	make -j"$(nproc)"
+    make -j"$(nproc)" benchmark UPPER_SHIFT="$SHIFT"
+
+    for REGEX in $REGEXES; do
+		COUNT=0
+		for NUM_OP in $NUM_OPS; do
+				for NUM_KEY in $NUM_KEYS; do
+						for ARG in $(seq 1 3 "$PERCENTAGES_COUNT"); do
+
+								INSERTION="$(  echo "$PERCENTAGES" | tr '\n' ' ' | awk "{print \$$((ARG + 0))}")"
+								SEARCH="$(     echo "$PERCENTAGES" | tr '\n' ' ' | awk "{print \$$((ARG + 1))}")"
+								ELIMINATION="$(echo "$PERCENTAGES" | tr '\n' ' ' | awk "{print \$$((ARG + 2))}")"
+
+								./bin/sepe-runner \
+										--verbose \
+										--distribution "normal" \
+										--operations "$NUM_OP" \
+										--keys "$NUM_KEY" \
+										--insert "$INSERTION" \
+										--search "$SEARCH" \
+										--elimination "$ELIMINATION" \
+										--repetitions $REPETITIONS \
+										--outfile "_${COUNT}_SHIFT_${SHIFT}.csv" \
+										"$REGEX"
+
+								COUNT=$((COUNT + 1))
+						done
+				done
+		done
+    done
+    mv -v ./*.csv temp-files/
+done
+zip -9 -o rq7-performance.zip -r temp-files/*
+
+# TODO: execute script
+
+git checkout artifact
+
+
